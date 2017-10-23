f203f068c1143496456554d1c3e119f8d424d89c
img21326 <img21326@gmail.com>
2017-10-23 20:46:53 +0800

    練習一 3a432018

---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <title>詳細資料</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}" />
+    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }} "></script>
+    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+</head>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+@include("layouts.head")
+<body>
+@include("layouts.nav")
+<div style="padding-top: 70px;"></div>
+<div class="container">
+    @yield("content")
+</div>
+</body>
+</html>
