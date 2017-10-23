a7fc7c33f9a56f9ef3ec018ab5c340077becdd48
img21326 <img21326@gmail.com>
2017-10-23 21:00:27 +0800

    edit studentcontroller add two funciton

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class StudentController extends Controller
 {
     //
-    public function getStudentData(){}
+    public function getStudentData($student_no){
+        return view('score',['student_no'=>$student_no,'subject'=>null]);
 
-    public function getStudentScore(){}
+    }
+
+    public function getStudentScore($student_no,$subject = null){
+        return view('score',['student_no'=>$student_no,'subject'=>$subject]);
+
+    }
 }
