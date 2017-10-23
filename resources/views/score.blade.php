5ff32efb67dde05a5302ee065ca87eb727d0a400
img21326 <img21326@gmail.com>
2017-10-23 20:47:21 +0800

    練習一(1) 3a432018

---
+++ resources/views/score.blade.php
@@ -0,0 +1,27 @@
+@extend("layouts.master")
+@section("content")
+    <div class="page-header">
+        <h1>詳細資料</h1>
+    </div>
+    <div class="row">
+        <p>
+            學號：123456
+        </p>
+        <p>
+            姓名：小明
+        </p>
+        <p>
+            電話：0912345678
+        </p>
+        <p>
+            國文：60
+        </p>
+        <p>
+            英文：60
+        </p>
+        <p>
+            數學：60
+        </p>
+        @endif
+    </div>
+@endsection
